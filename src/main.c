ca934d47fe03810116f57b29550f440662cccf84
Okamoto-Taiyo02-TP <282410817+Okamoto-Taiyo02-TP@users.noreply.github.com>
2026-05-19 09:41:36 +0900

    switch文を入れたら動かなくなりました

--- src/main.c
+++ src/main.c
@@ -13,12 +13,22 @@ int main(void) {
     int user_input;
     do {
         printf("=== メニュー ===\n");
+        Drink();
+        printf("=== 操作 ===\n");
         printf("1: お金を入れる\n");
         printf("9: 終了\n");
         if(scanf("%d", &user_input) != 1){
             printf("error: 半角数字で入力してください。\n");            
             while (getchar() != '\n');
             continue;
+        }
+        switch(user_input) {
+            case 1:
+                handle_deposit();
+                break;
+            default:
+                printf("無効な選択です。もう一度入力してください。\n");
+                break;
         }
     } while (user_input != 9);
     return 0;
